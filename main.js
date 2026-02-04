afa1e55aebe2d1dc506a7ecee922cd7d21843e15
SebDVS Web <69651784+sebdvsweb@users.noreply.github.com>
2026-02-04 13:01:43 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,24 @@
-const url = "https://tyradex.app/api/v1/gen/2";
+const url = "https://tyradex.app/api/v1/gen/1";
 
 const mesPokemon = fetch(url);
  
 mesPokemon.then(function(response) {
 
     // stocker la reponse json dans une variable
-    const pokedex = response.json();
 
     // faire un then qui initialise une fonction inclant un forEach pour parcourir l'API
-    pokedex.then(function(data){
+    
+    // Extraire les informations nécessaires
+     
+    // Sélectionner le conteneur HTML principal
+     
+    // Créer la card
+    
+    // Ajouter l'image, le titre, les types
 
-        data.forEach(function(pokemon){
+    // Ajouter les éléments à la carte
 
-           /*
-           
-           if (pokemon.pokedex_id === 0) {
-                return;
-            }
-
-            */
-
-            // Extraire les informations nécessaires
-            const numero = pokemon.pokedex_id; // on récupère l'ID
-            const nom = pokemon.name.fr; // on récupère le nom français
-            const image = pokemon.sprites.regular; // on récupère l'image classique
-            const types = pokemon.types.map(type => type.name).join(", "); // Plante, Poison
-
-            // Sélectionner le conteneur HTML principal
-            let container = document.querySelector('.pokemon-list');
-                
-            // Créer la card
-            let card = document.createElement('div'); // <div></div>
-            card.className = "pokemon-card";
-
-            // Ajouter l'image, le titre, les types
-            let imgPokemon = document.createElement('img'); // </img>
-            imgPokemon.src = image;
-            imgPokemon.style.width = "100%";
-
-            let nomPokemon = document.createElement('h4'); // <h4></h4>
-            nomPokemon.textContent = '#' + numero + ' ' + nom; // #1 Bulbizarre
-
-            let typePokemon = document.createElement('p') // <p></p>
-            typePokemon.textContent = 'Type(s) : ' + types;
-
-            // Ajouter les éléments à la carte
-            card.appendChild(imgPokemon);
-            card.appendChild(nomPokemon);
-            card.appendChild(typePokemon);
-
-            // Ajouter la carte dans le conteneur principal
-            container.appendChild(card);
-
-        })
-
-    })
+    // Ajouter la carte dans le conteneur principal
      
 })
     // Gérer les erreurs
